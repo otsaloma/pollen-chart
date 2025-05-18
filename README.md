816c5a89f81f5035d8af524e1f01d207dd0d1d45
Osmo Salomaa <otsaloma@iki.fi>
2025-05-18 22:28:48 +0300

    Implement visualization

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 Pollen Chart
 ============
 
-A visualization of the pollen situation in Helsinki in two parts: (1) A
-lambda function that downloads data from [CAMS][] using [CDSAPI][] to an
-S3 bucket and (2) a static client-side web app that renders that data
-into a custom visualization.
+A visualization of pollen counts in Helsinki in two parts: (1) A lambda
+function that downloads data from [CAMS][] using [CDSAPI][] to an S3
+bucket and (2) a static client-side web app that renders that data into
+a custom visualization.
 
 [CAMS]: https://ads.atmosphere.copernicus.eu/datasets/cams-europe-air-quality-forecasts
 [CDSAPI]: https://ads.atmosphere.copernicus.eu/how-to-api
--- main.js
+++ main.js
@@ -1,13 +1,84 @@
 // -*- coding: utf-8-unix -*-
 
-function renderChart(data) {
+// Lower limits of "abundant" level as per norkko.fi (grains/m3)
+// https://norkko.fi/usein-kysytyt-kysymykset/
+const YMAX_DEFAULT_LEVELS = {
+    alder:   100,
+    birch:   100,
+    grass:    30,
+    mugwort:  30,
+    ragweed:  30,
+};
+
+function renderRow(data, key, label) {
+    const chart = document.getElementById("chart");
+    const last = data[data.length-1];
+
+    // Label
+    var div = document.createElement("div");
+    div.classList.add("align-left");
+    div.innerHTML = label;
+    chart.appendChild(div);
+
+    // Bar
+    // Render the Y-axis maximum at YMAX_DEFAULT_LEVELS
+    // or maximum seen in the data, whichever is greater.
+    var div = document.createElement("div");
+    div.classList.add("bars");
+    div.style.gridTemplateColumns = `repeat(${data.length}, 1fr)`;
+    var ref = data.reduce((ymax, y) => Math.max(ymax, y[key]), 0);
+    ref = Math.max(YMAX_DEFAULT_LEVELS[key], ref);
+    for (var item of data) {
+        var bar = document.createElement("div");
+        bar.classList.add("bar");
+        var height = item[key] / ref;
+        bar.style.height = `${100*item[key]/ref}%`;
+        div.appendChild(bar);
+    }
+    chart.appendChild(div);
+
+    // Percent change
+    // Compare the latest date against mean of the previous seven.
+    var div = document.createElement("div");
+    div.classList.add("align-right");
+    var prev = data.slice(-8, -1);
+    var ref = prev.reduce((total, x) => total + x[key], 0) / prev.length;
+    var value = (Math.max(1, last[key]) - Math.max(1, ref)) / Math.max(1, ref);
+    sign = value === 0 ? "" : (value < 0 ? "–" : "+");
+    div.innerHTML = `${sign}${Math.abs(100*value).toFixed(0)}%`;
+    chart.appendChild(div);
+
+    // Value (grains/m3)
+    var div = document.createElement("div");
+    div.classList.add("align-right");
+    var value = Number(last[key].toPrecision(2));
+    div.innerHTML = `${value.toFixed(0)}`;
+    chart.appendChild(div);
 
 }
 
-(function() {
+function formatDate(string) {
+    const date = new Date(string);
+    const weekday = ["su", "ma", "ti", "ke", "to", "pe", "la"][date.getDay()];
+    return `${weekday} ${date.getDate()}.${date.getMonth()+1}.${date.getFullYear()}`;
+}
 
+function renderChart(data) {
+    data = data.slice(-8);
+    const p = document.getElementById("intro");
+    const date = data[data.length-1].date;
+    intro.innerHTML = `Tilanne ${formatDate(date)}`;
+    // In chronological order of appearance
+    // https://www.siitepolytieto.fi/mika-allergia/siitepolykausi
+    renderRow(data, "alder", "Leppä");
+    renderRow(data, "birch", "Koivu");
+    renderRow(data, "grass", "Heinä");
+    renderRow(data, "mugwort", "Pujo");
+    renderRow(data, "ragweed", "Tuoksukki");
+}
+
+(function() {
     fetch("helsinki.json")
         .then(response => response.json())
         .then(data => renderChart(data));
-
 })();
